39ec448bdf749a6dec8446019f0754ebdf718b6d
Stan Mur <practikum-2022@yandex.ru>
2023-01-25 17:42:53 +0300

    second iteration

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
 // модалка
-let popup = document.querySelector(".popup");
+let popup = document.querySelector('.popup');
 
 //тайтлы
-let profileTitle = document.querySelector(".profile__title");
-let profileSubtitle = document.querySelector(".profile__subtitle");
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
 
 // Находим форму в DOM (и инпуты)
-let formElement = document.querySelector(".popup__form");
+let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.elements.name;
 let jobInput = formElement.elements.about;
 
 //открытие модалки
-let btnPopupOpened = document.querySelector(".profile__edit-btn");
+let btnPopupOpened = document.querySelector('.profile__edit-btn');
 function popupOpened() {
-  popup.classList.toggle("popup_opened");
+  popup.classList.toggle('popup_opened');
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
 }
 
 //закрытие модалки
-let btnPopupClose = document.querySelector(".popup__close-btn");
+let btnPopupClose = document.querySelector('.popup__close-btn');
 function popupClose() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
 
-// бытон нарезной сохранить
+// батон нарезной сохранить
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
@@ -34,7 +34,7 @@ function handleFormSubmit(evt) {
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener("submit", handleFormSubmit);
+formElement.addEventListener('submit', handleFormSubmit);
 
-btnPopupOpened.addEventListener("click", popupOpened); //открыли
-btnPopupClose.addEventListener("click", popupClose); //закрыли
+btnPopupOpened.addEventListener('click', popupOpened); //открыли
+btnPopupClose.addEventListener('click', popupClose); //закрыли
